bf292fd5d64812f576f4e1f3459845789e7876f2
Ramprakash Yadav <ramprakashyadav2027@gmail.com>
2025-05-24 11:25:25 +0530

    /

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const[visible,setvisible]=useState(false);
 
   return (
     <div className="flex  items-center justify-between py-5 font-medium">
-      <img src={assets.logo} className="w-36" alt="" />
+     <Link to="/"> <img src={assets.logo} className="w-36" alt="" /></Link>
       <ul className="hidden sm:flex gap-5 test-sm text-gray-700">
         <NavLink
           to="/"
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import Hero from '../components/Hero'
 import Latestcol from '../components/Latestcol'
+import Bestseller from '../components/Bestseller'
+import Ourpolices from '../components/Ourpolices'
+import Newletterbox from '../components/Newletterbox'
 
 function Home() {
   return (
     <div>
      <Hero/>
      <Latestcol/>
+     <Bestseller/>
+     <Ourpolices/>
+     <Newletterbox/>
     </div>
   )
 }
